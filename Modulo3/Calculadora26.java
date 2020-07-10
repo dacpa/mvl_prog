9e7c980b18a8826710790a6b1bff45abf2089954
dacpa <dpozzoardizzi@gmail.com>
2020-07-10 11:52:29 -0300

    Se agrega la tarea de la Clase 26 - Módulo 3.

---
+++ Modulo3/Calculadora26.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mvl.matematica;
+
+//Tarea 26 - Módulo 3 - Seguimos reforzando conceptos básico.
+//Alumno: Daniel Pozzo Ardizzi.
+
+public class Calculadora26 {
+ //atributos
+    private float num1;
+    private float num2;
+    private String nombreUsuario;
+    
+    //constructor
+
+    public Calculadora26() {
+    }
+
+    public Calculadora26(float num1, float num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    //métodos
+    //los geter permiten mostrar la información (los atributos de la clase) y los seter permiten agregar o cambiar la información
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public float getNum1() {
+        return num1;
+    }
+
+    public void setNum1(float num1) {
+        this.num1 = num1;
+    }
+
+    public float getNum2() {
+        return num2;
+    }
+
+    public void setNum2(float num2) {
+        this.num2 = num2;
+    }
+    public float sumarNumeros (){
+    //variable auxiliar que existe solo en el método calcularArea
+      float sumar;
+     sumar = (this.num1 + this.num2);
+     return sumar;
+    }
+     public float restarNumeros (){
+    //variable auxiliar que existe solo en el método calcularArea
+      float restar;
+     restar = (this.num1 - this.num2);
+     return restar;
+    }
+    public float multiplicarNumeros (){
+    //variable auxiliar que existe solo en el método calcularArea
+      float multiplicar;
+     multiplicar = (this.num1 * this.num2);
+     return multiplicar;
+    }
+    public float dividirNumeros (){
+    //variable auxiliar que existe solo en el método calcularArea
+      float dividir;
+     dividir = (this.num1 / this.num2);
+     return dividir;
+    }
+
+}
---
+++ Modulo3/Principal26.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mvl.matematica;
+
+//Importar únicamente la librería especifica de JOptionPane.
+import javax.swing.JOptionPane;
+
+//Alumno: Daniel Pozzo Ardizzi.
+
+public class Principal26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    String nombreUsuario = JOptionPane.showInputDialog("Ingrese su nombre");
+    JOptionPane.showMessageDialog(null,"Buen día "+nombreUsuario);
+   
+            
+    String primerNumero = JOptionPane.showInputDialog("Ingrese el primer número para calcular");
+    String segundoNumero = JOptionPane.showInputDialog("Ingrese el segundo número para calcular");
+    int numero1 = Integer.parseInt(primerNumero);
+    int numero2 = Integer.parseInt(segundoNumero);
+    
+    System.out.println ("");
+    System.out.println ("**********************************************");
+    System.out.println ("Tarea 26 - Módulo 3  - Diálogo con el usuario.");
+    System.out.println ("**********************************************");
+    System.out.println ("");
+    
+    Calculadora26 c1=new Calculadora26(numero1,numero2);
+    System.out.println("Calculadora");
+    System.out.println ("");
+    System.out.println("Los datos son:");
+    System.out.println ("");
+    System.out.println("Primer número = "+c1.getNum1()+".");
+    System.out.println("Segundo número = "+c1.getNum2()+".");
+    System.out.println("");
+    
+    JOptionPane.showMessageDialog(null,"El resultado de la suma es: "+c1.sumarNumeros());
+    JOptionPane.showMessageDialog(null,"El resultado de la resta es: "+c1.restarNumeros());
+    JOptionPane.showMessageDialog(null,"El resultado de la multiplicación es: "+c1.multiplicarNumeros());
+   
+            if(c1.dividirNumeros()==Float.POSITIVE_INFINITY || c1.dividirNumeros()==Float.NEGATIVE_INFINITY){
+            System.out.println("ERROR!");
+        }else{
+            JOptionPane.showMessageDialog(null,"El resultado de la división es: " +c1.dividirNumeros());
+           
+         
+    System.out.println ("");
+    System.out.println ("");
+    System.out.println ("Alumno: Daniel Pozzo Ardizzi.");
+    System.out.println ("");
+    System.out.println ("");
+    }
+    
+ }
+
+  
+    }
+     
